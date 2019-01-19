644d2100e5500cdf36ee2c485d0fffee875d6684
unknown <kamil.kurek@onet.eu>
2019-01-19 21:01:09 +0100

    Controllers fixes.

--- src/test/java/pl/kkp/core/controller/TestMainController.java
+++ src/test/java/pl/kkp/core/controller/TestMainController.java
@@ -1,24 +1,17 @@
 package pl.kkp.core.controller;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import pl.kkp.core.testing.TestRestController;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 
 public class TestMainController extends TestRestController {
 
-    @Autowired
-    private MainController mainController;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
     @Test
     public void isIndexPageResponse() {
-        String response = restTemplate.getForObject(TestRestController.APPLICATION_ADDR, String.class);
+        String endpointPath = getEndpointAddress("");
+        String response = restTemplate.getForObject(endpointPath, String.class);
 
         String expectedResponse = MainController.INDEX_MESSAGE;
         assertThat(response).isEqualTo(expectedResponse);
--- src/test/java/pl/kkp/core/testing/TestRestController.java
+++ src/test/java/pl/kkp/core/testing/TestRestController.java
@@ -1,12 +1,27 @@
 package pl.kkp.core.testing;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
-public abstract class TestRestController {
-    public static final Integer APPLICATION_PORT = 8080;
-    public static final String APPLICATION_ADDR = "http://localhost:" + APPLICATION_PORT;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public abstract class TestRestController extends SpringBootBaseTest {
+    private static final String APP_ADDRESS = "http://localhost";
+
+    @Autowired
+    protected TestRestTemplate restTemplate;
+
+    protected void assertResponseStatusCode(ResponseEntity response) {
+        assertThat(response).isNotNull();
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    }
+
+
+    protected String getEndpointAddress(String addressPath) {
+        String pathDelimiter = addressPath.startsWith("/") ? "" : "/";
+
+        return APP_ADDRESS + ":" + portNumber + pathDelimiter + addressPath;
+    }
 }
